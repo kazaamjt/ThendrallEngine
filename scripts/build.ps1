3af7e685c3a7b89d00b47814749f53244e7e6e76
kazaamjt <kazaamjt@gmail.com>
2021-08-01 18:45:05 +0200

    house keeping

--- scripts/build.ps1
+++ scripts/build.ps1
@@ -1,7 +1,7 @@
 param(
 	[parameter(mandatory=$false)]
 	[string]$Name="main",
-	[ValidateSet('msvc', 'gcc', 'clang')]
+	[ValidateSet('msvc', 'clang')]
 	[string]$Compiler='clang',
 
 	[switch]$Clean=$false,
@@ -17,17 +17,13 @@ if ($Clean) {
 }
 
 if ($GenerateAssets) {
-	.env\Scripts\python.exe .\tools\texture_generator\src\main.py --in .\art --out .\data\textures
+	.env\Scripts\python.exe .\tools\texture_generator\main.py --in .\art --out .\data\textures
 }
 
 if ($Compiler -eq 'clang') {
 	bazel build $Name --config=clang-cl --compiler=clang-cl --keep_going
-}
-
-else {
+} else {
 	bazel build $Name --config=cl --keep_going
 }
-Copy-Item lib/GL/win32/glew32.dll bazel-bin/glew32.dll
-Copy-Item lib/SDL2/win32/SDL2.dll bazel-bin/SDL2.dll
 
 Set-Location $Return
--- scripts/init.ps1
+++ scripts/init.ps1
@@ -1,24 +1,16 @@
-param(
-	# Install the dev requirements of the tools
-	[switch]$InstallDevTools
-)
-
 $Return = Get-Location
 Set-Location "$PSScriptRoot\.."
 
-# Init submodules
-git submodule init
-git submodule update
-
 # init tools
 if (!(Test-Path .env)) {
+	Write-Output "Creating python venv"
 	python.exe -m venv .env
 }
 .env\Scripts\python -m pip install pip --upgrade
-.env\Scripts\pip.exe install -r .\tools\texture_generator\requirements.txt
 
-if ($InstallDevTools) {
-	.env\Scripts\pip.exe install -r .\tools\texture_generator\requirements.dev.txt
-}
+mkdir tools/texture_generator -Force
+Invoke-WebRequest https://raw.githubusercontent.com/kazaamjt/Texture-Generator/master/src/main.py -UseBasicParsing -OutFile tools/texture_generator/main.py
+Invoke-WebRequest https://raw.githubusercontent.com/kazaamjt/Texture-Generator/master/requirements.txt -UseBasicParsing -OutFile tools/texture_generator/requirements.txt
+.env\Scripts\pip.exe install -r .\tools\texture_generator\requirements.txt
 
 Set-Location $Return
